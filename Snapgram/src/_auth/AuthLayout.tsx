72a03c97815436d7f85fe333ca5016e57d7c9b0d
John Doe <johndoe@email.com>
2023-12-13 17:58:39 +0100

    17:58 13.12.23

--- Snapgram/src/_auth/AuthLayout.tsx
+++ Snapgram/src/_auth/AuthLayout.tsx
@@ -9,12 +9,18 @@ const AuthLayout = () => {
         <Navigate to="/" />
       ) : (
         <>
-          <section className="flex flex-1 justify-center items-center
-          flex-col py-10"> 
+          <section
+            className="flex flex-1 justify-center items-center
+          flex-col py-10"
+          >
             <Outlet />
           </section>
 
-          <img/>
+          <img
+            src="\assets\images\side-img.svg"
+            alt="logo"
+            className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
+          />
         </>
       )}
     </>
